491a6396cece3c4b2454827ac3a584d24451498a
kacper331 <denebv201@gmail.com>
2015-08-17 21:35:16 +0200

    Fix label of field in sign_up_with in session_helper method

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -3,7 +3,7 @@ module Features
     def sign_up_with(email, password)
       visit new_user_registration_path
       fill_in 'Email', with: email
-      fill_in 'user_password', with: password
+      fill_in 'Password', with: password
       fill_in 'Password confirmation', with: password
       within(".form-actions") do
         click_on 'Sign up'
