87746d853fb115b3a443a93ce16ad48d0a603740
kacper331 <denebv201@gmail.com>
2015-08-17 21:33:46 +0200

    Add student_id column to SubjectItem model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817152304) do
+ActiveRecord::Schema.define(version: 20150817161428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150817152304) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "teacher_id"
+    t.integer  "student_id"
   end
 
   add_index "subject_items", ["teacher_id"], name: "index_subject_items_on_teacher_id", using: :btree
