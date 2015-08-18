553731e4de8ba26399b187162f242c4489b8ac7e
kacper331 <denebv201@gmail.com>
2015-08-18 11:31:15 +0200

    Change path in after_sign_up_path_for method.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    visitors_index_path(resource)
+    root_path(resource)
   end
 end
