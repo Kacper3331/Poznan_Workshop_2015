42e447fed9cd4235ba179aae96f279a7707f0819
kacper331 <denebv201@gmail.com>
2015-08-19 10:59:18 +0200

    Change seeds data to match task requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,43 @@
 puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
-User.create!(email: 'admin@admin.com',password: 'adminadmin')
-
-30.times do
+User.create!(email: 'admin@admin.com',password: 'admin123')
+puts "Start Create Teachers"
+3.times do
   Teacher.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     academic_title: TEACHER_TITLES.sample
   )
 end
+puts "Finish Create Teachers"
 
+puts "Start Create Subjects"
 teachers = Teacher.all
-20.times do
+5.times do
   SubjectItem.create!(
     title: Faker::Lorem.sentence,
     teacher: teachers.sample
   )
 end
+puts "Finish Create Subjects"
 
-40.times do
+puts "Start Create Students"
+25.times do
   Student.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name
   )
 end
+puts "Finish Create Students"
 
+puts "Start Associate Students with Subjects"
 students = Student.all
 SubjectItem.all.each do |subject_item|
   subject_item.students << students.sample(rand(1..4))
 end
+puts "Finish Associate Students with Subjects"
 
+puts "Start Create and Associate SubjectItemNotes with Subjects"
 SubjectItem.all.each do |subject_item|
   subject_item.students.each do |student|
     rand(1..5).times do
@@ -39,5 +47,6 @@ SubjectItem.all.each do |subject_item|
     end
   end
 end
+puts "Finish Create and Associate SubjectItemNotes with Subjects"
 
 puts "Seeds: done"
