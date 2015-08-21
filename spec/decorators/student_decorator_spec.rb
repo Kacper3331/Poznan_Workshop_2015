7d896b40c01cde8a49cd0f44fd83bc2414d10b24
kacper331 <denebv201@gmail.com>
2015-08-21 14:06:26 +0200

    Add test to check new created birth_date decorator method

--- spec/decorators/student_decorator_spec.rb
+++ spec/decorators/student_decorator_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe StudentDecorator do
   let(:teacher) { create :teacher, first_name: 'John', last_name: 'Smith' }
-  let(:student) { create :student, first_name: 'John', last_name: 'Smith' }
+  let(:student) { create :student, first_name: 'John', last_name: 'Smith', birthdate: '12/12/1999' }
   let(:subject_item) { create :subject_item, teacher: teacher }
   let(:second_subject_item) { create :subject_item }
   let!(:note_1) { create :subject_item_note, value: 5, student: student, subject_item: second_subject_item }
@@ -11,6 +11,11 @@ describe StudentDecorator do
   describe "#full_name" do
     subject { student.decorate.full_name }
     it { is_expected.to eq 'John Smith' }
+  end
+
+  describe "#birt_date" do
+    subject { student.decorate.birth_date }
+    it { is_expected.to eq '1999-12-12' }
   end
 
   describe "#avg_notes" do
