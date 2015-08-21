c134d347757d8a97eeaf26810ef78802e53591d2
kacper331 <denebv201@gmail.com>
2015-08-21 13:56:50 +0200

    Create birth_date method in student decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -3,6 +3,10 @@ class StudentDecorator < BaseDecorator
     "#{first_name} #{last_name}"
   end
 
+  def birth_date
+    "#{birthdate}"
+  end
+
   def avg_notes(subject_item)
     average = SubjectItemNote.where(subject_item_id: subject_item.id).average(:value)
     average.nil? ? '0.00' : "%.2f" % average
